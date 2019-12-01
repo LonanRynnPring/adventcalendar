9aab0ed5462170001d563a27df42efbd7fc66e2f
lonan rynn-pring <lonanrynnpring@gmail.com>
2019-12-01 00:02:36 +0000

    added sound

---
+++ audio.js
@@ -0,0 +1,433 @@
+var one = document.getElementById('one');
+var onea = document.getElementById('day-1');
+var oneb = false;
+onea.addEventListener("click", function playAudio() {
+    if (oneb){
+        one.pause();
+    }
+    else {
+        one.play();
+    }
+});
+one.onplaying = function() {
+    oneb = true;
+}
+one.onpause = function() {
+    oneb = false;
+}
+
+var two = document.getElementById('two');
+var twoa = document.getElementById('day-2');
+var twob = false;
+twoa.addEventListener("click", function playAudio() {
+    if (twob){
+        two.pause();
+    }
+    else {
+        two.play();
+    }
+});
+two.onplaying = function() {
+    twob = true;
+}
+two.onpause = function() {
+    twob = false;
+}
+
+var three = document.getElementById('three');
+var threea = document.getElementById('day-3');
+var threeb = false;
+threea.addEventListener("click", function playAudio() {
+    if (threeb){
+        three.pause();
+    }
+    else {
+        three.play();
+    }
+});
+three.onplaying = function() {
+    threeb = true;
+}
+three.onpause = function() {
+    threeb = false;
+}
+
+var four = document.getElementById('four');
+var foura = document.getElementById('day-4');
+var fourb = false;
+foura.addEventListener("click", function playAudio() {
+    if (fourb){
+        four.pause();
+    }
+    else {
+        four.play();
+    }
+});
+four.onplaying = function() {
+    fourb = true;
+}
+four.onpause = function() {
+    fourb = false;
+}
+
+var five = document.getElementById('five');
+var fivea = document.getElementById('day-5');
+var fiveb = false;
+fivea.addEventListener("click", function playAudio() {
+    if (fiveb){
+        five.pause();
+    }
+    else {
+        five.play();
+    }
+});
+five.onplaying = function() {
+    fiveb = true;
+}
+five.onpause = function() {
+    fiveb = false;
+}
+
+var six = document.getElementById('six');
+var sixa = document.getElementById('day-6');
+var sixb = false;
+sixa.addEventListener("click", function playAudio() {
+    if (sixb){
+        six.pause();
+    }
+    else {
+        six.play();
+    }
+});
+six.onplaying = function() {
+    sixb = true;
+}
+six.onpause = function() {
+    sixb = false;
+}
+
+var seven = document.getElementById('seven');
+var sevena = document.getElementById('day-7');
+var sevenb = false;
+sevena.addEventListener("click", function playAudio() {
+    if (sevenb){
+        seven.pause();
+    }
+    else {
+        seven.play();
+    }
+});
+seven.onplaying = function() {
+    sevenb = true;
+}
+seven.onpause = function() {
+    sevenb = false;
+}
+
+var eight = document.getElementById('eight');
+var eighta = document.getElementById('day-8');
+var eightb = false;
+eighta.addEventListener("click", function playAudio() {
+    if (eightb){
+        eight.pause();
+    }
+    else {
+        eight.play();
+    }
+});
+eight.onplaying = function() {
+    eightb = true;
+}
+eight.onpause = function() {
+    eightb = false;
+}
+
+var nine = document.getElementById('nine');
+var ninea = document.getElementById('day-9');
+var nineb = false;
+ninea.addEventListener("click", function playAudio() {
+    if (nineb){
+        nine.pause();
+    }
+    else {
+        nine.play();
+    }
+});
+nine.onplaying = function() {
+    nineb = true;
+}
+nine.onpause = function() {
+    nineb = false;
+}
+
+var ten = document.getElementById('ten');
+var tena = document.getElementById('day-10');
+var tenb = false;
+tena.addEventListener("click", function playAudio() {
+    if (tenb){
+        ten.pause();
+    }
+    else {
+        ten.play();
+    }
+});
+ten.onplaying = function() {
+    tenb = true;
+}
+ten.onpause = function() {
+    tenb = false;
+}
+
+var eleven = document.getElementById('eleven');
+var elevena = document.getElementById('day-11');
+var elevenb = false;
+elevena.addEventListener("click", function playAudio() {
+    if (elevenb){
+        eleven.pause();
+    }
+    else {
+        eleven.play();
+    }
+});
+eleven.onplaying = function() {
+    elevenb = true;
+}
+eleven.onpause = function() {
+    elevenb = false;
+}
+
+var twelve = document.getElementById('twelve');
+var twelvea = document.getElementById('day-12');
+var twelveb = false;
+twelvea.addEventListener("click", function playAudio() {
+    if (twelveb){
+        twelve.pause();
+    }
+    else {
+        twelve.play();
+    }
+});
+twelve.onplaying = function() {
+    twelveb = true;
+}
+twelve.onpause = function() {
+    twelveb = false;
+}
+
+var thirteen = document.getElementById('thirteen');
+var thirteena = document.getElementById('day-13');
+var thirteenb = false;
+thirteena.addEventListener("click", function playAudio() {
+    if (thirteenb){
+        thirteen.pause();
+    }
+    else {
+        thirteen.play();
+    }
+});
+thirteen.onplaying = function() {
+    thirteenb = true;
+}
+thirteen.onpause = function() {
+    thirteenb = false;
+}
+
+var fourteen = document.getElementById('fourteen');
+var fourteena = document.getElementById('day-14');
+var fourteenb = false;
+fourteena.addEventListener("click", function playAudio() {
+    if (fourteenb){
+        fourteen.pause();
+    }
+    else {
+        fourteen.play();
+    }
+});
+fourteen.onplaying = function() {
+    fourteenb = true;
+}
+fourteen.onpause = function() {
+    fourteenb = false;
+}
+
+var fifteen = document.getElementById('fifteen');
+var fifteena = document.getElementById('day-15');
+var fifteenb = false;
+fifteena.addEventListener("click", function playAudio() {
+    if (fifteenb){
+        fifteen.pause();
+    }
+    else {
+        fifteen.play();
+    }
+});
+fifteen.onplaying = function() {
+    fifteenb = true;
+}
+fifteen.onpause = function() {
+    fifteenb = false;
+}
+
+var sixteen = document.getElementById('sixteen');
+var sixteena = document.getElementById('day-16');
+var sixteenb = false;
+sixteena.addEventListener("click", function playAudio() {
+    if (sixteenb){
+        sixteen.pause();
+    }
+    else {
+        sixteen.play();
+    }
+});
+sixteen.onplaying = function() {
+    sixteenb = true;
+}
+sixteen.onpause = function() {
+    sixteenb = false;
+}
+
+var seventeen = document.getElementById('seventeen');
+var seventeena = document.getElementById('day-17');
+var seventeenb = false;
+seventeena.addEventListener("click", function playAudio() {
+    if (seventeenb){
+        seventeen.pause();
+    }
+    else {
+        seventeen.play();
+    }
+});
+seventeen.onplaying = function() {
+    seventeenb = true;
+}
+seventeen.onpause = function() {
+    seventeenb = false;
+}
+
+var eighteen = document.getElementById('eighteen');
+var eighteena = document.getElementById('day-18');
+var eighteenb = false;
+eighteena.addEventListener("click", function playAudio() {
+    if (eighteenb){
+        eighteen.pause();
+    }
+    else {
+        eighteen.play();
+    }
+});
+eighteen.onplaying = function() {
+    eighteenb = true;
+}
+eighteen.onpause = function() {
+    eighteenb = false;
+}
+
+var nineteen = document.getElementById('nineteen');
+var nineteena = document.getElementById('day-19');
+var nineteenb = false;
+nineteena.addEventListener("click", function playAudio() {
+    if (nineteenb){
+        nineteen.pause();
+    }
+    else {
+        nineteen.play();
+    }
+});
+nineteen.onplaying = function() {
+    nineteenb = true;
+}
+nineteen.onpause = function() {
+    nineteenb = false;
+}
+
+var twenty = document.getElementById('twenty');
+var twentya = document.getElementById('day-20');
+var twentyb = false;
+twentya.addEventListener("click", function playAudio() {
+    if (twentyb){
+        twenty.pause();
+    }
+    else {
+        twenty.play();
+    }
+});
+twenty.onplaying = function() {
+    twentyb = true;
+}
+twenty.onpause = function() {
+    twentyb = false;
+}
+
+var twentyone = document.getElementById('twentyone');
+var twentyonea = document.getElementById('day-21');
+var twentyoneb = false;
+twentyonea.addEventListener("click", function playAudio() {
+    if (twentyoneb){
+        twentyone.pause();
+    }
+    else {
+        twentyone.play();
+    }
+});
+twentyone.onplaying = function() {
+    twentyoneb = true;
+}
+twentyone.onpause = function() {
+    twentyoneb = false;
+}
+
+var twentytwo = document.getElementById('twentytwo');
+var twentytwoa = document.getElementById('day-22');
+var twentytwob = false;
+twentytwoa.addEventListener("click", function playAudio() {
+    if (twentytwob){
+        twentytwo.pause();
+    }
+    else {
+        twentytwo.play();
+    }
+});
+twentytwo.onplaying = function() {
+    twentytwob = true;
+}
+twentytwo.onpause = function() {
+    twentytwob = false;
+}
+
+var twentythree = document.getElementById('twentythree');
+var twentythreea = document.getElementById('day-23');
+var twentythreeb = false;
+twentythreea.addEventListener("click", function playAudio() {
+    if (twentythreeb){
+        twentythree.pause();
+    }
+    else {
+        twentythree.play();
+    }
+});
+twentythree.onplaying = function() {
+    twentythreeb = true;
+}
+twentythree.onpause = function() {
+    twentythreeb = false;
+}
+
+var twentyfour = document.getElementById('twentyfour');
+var twentyfoura = document.getElementById('day-24');
+var twentyfourb = false;
+twentyfoura.addEventListener("click", function playAudio() {
+    if (twentyfourb){
+        twentyfour.pause();
+    }
+    else {
+        twentyfour.play();
+    }
+});
+twentyfour.onplaying = function() {
+    twentyfourb = true;
+}
+twentyfour.onpause = function() {
+    twentyfourb = false;
+}
+
+
